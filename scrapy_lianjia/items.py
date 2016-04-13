458939dfbd81fcea0c5773159d4cc87f8f3a38e7
Curtis Yu <cuyu@splunk.com>
2016-04-13 17:19:39 +0800

    Start crawl from main page.

--- scrapy_lianjia/items.py
+++ scrapy_lianjia/items.py
@@ -12,3 +12,5 @@ class LianjiaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     price = scrapy.Field()
+    room = scrapy.Field()
+    area = scrapy.Field()
--- scrapy_lianjia/spiders/lianjia_spider.py
+++ scrapy_lianjia/spiders/lianjia_spider.py
@@ -13,10 +13,15 @@ class LianjiaSpider(scrapy.Spider):
     name = "lianjia"
     allowed_domains = ["lianjia.com"]
     start_urls = [
-        "http://sh.lianjia.com/ershoufang/sh1939876.html"
+        "http://sh.lianjia.com/ershoufang"
     ]
 
     def parse(self, response):
+        for house_info in response.xpath("//ul[@id='house-lst']/li"):
+            url = response.urljoin(house_info.xpath("div/a/@href").extract()[0])
+            yield scrapy.Request(url, callback=self.parse_house_info)
+
+    def parse_house_info(self, response):
         sel_houseinfo = response.css("body > div.esf-top > div.cj-cun > div.content > div.houseInfo")
         item = LianjiaItem()
         item['price'] = sel_houseinfo.css("div.price > div").extract()
